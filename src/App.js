25ec0a9efb55b7bbb727cc604a8d8f91bcb75e4c
Rahul R Soni <110813609+rahulsonisoni2015@users.noreply.github.com>
2022-12-01 16:16:02 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import './App.css';
+// import About from './component/About';
+import Navbar from './component/Navbar';
+import Textform from './component/Textform';
+import React, { useState } from 'react'
+import Alert from './component/Alert';
+
+function App() {
+  const [mode, setMode] = useState('light');
+
+  const [alert, setAlert] = useState(null)
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 1500);
+  }
+
+  const tonggleMode = () => {
+    if (mode === 'light') {
+      setMode("dark")
+      document.body.style.backgroundColor = "#1a1d45"
+      showAlert(", Dark mode has been on", "success")
+    } else {
+      setMode("light")
+      document.body.style.backgroundColor = "#aeedfc"
+      showAlert(", Dark mode has been off", "success")
+    }
+  }
+
+  return (
+    <>
+      <Navbar title="Text tool" about="About us" mode={mode} tonggleMode={tonggleMode} />
+      <Alert alert={alert} />
+      <div className='container my-3'>
+            {/* <About /> */}
+            <Textform heading='Enter a text here' showAlert={showAlert} mode={mode} />
+      </div>
+  </>
+  );
+}
+
+export default App;
---
+++ src/component/Textform.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react'
+
+
+export default function Textform(props) {
+    const [text, setText] = useState("");
+
+    const hendleOnClick = () => {
+        // console.log("clicked");
+        let newText = text.toUpperCase();
+        setText(newText);
+        props.showAlert(", Conveted to uppercase", "success");
+    }
+    const hendleLoClick = () => {
+        // console.log("clicked");
+        let newText = text.toLowerCase();
+        setText(newText);
+        props.showAlert(", Conveted to lowercase", "success");
+    }
+
+    const hendleSetCase = () => {
+        let newText = text.replace(/(?<=(?:^|[.?!])\W*)[a-z]/g, i => i.toUpperCase());;
+        setText(newText);
+        props.showAlert(", Conveted to standerdcase", "success");
+    }
+
+    const hendleClear = () => {
+        let newText = "";
+        setText(newText);
+        props.showAlert(", Text has been clear", "success");
+    }
+
+    const hendletrim = () => {
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "));
+        props.showAlert(", Removed all speces", "success");
+    }
+
+    const hendleCopy = () => {
+        navigator.clipboard.writeText(text);
+    }
+
+    const hendleOnChange = (event) => {
+        // console.log("onchange");
+        setText(event.target.value)
+    }
+
+
+    return (
+        <>
+            <div className='Container' style={{ color: props.mode === 'dark' ? 'white' : '#1a1d45' }}>
+                <div className="mb-3">
+                    <label htmlFor="myBox" className="form-label"><h2>{props.heading}</h2></label>
+                    <textarea className="form-control" style={{ backgroundColor: props.mode === 'dark' ? '#666666' : '#faf9f7', color: props.mode === 'dark' ? 'white' : 'black' }} id="myBox" value={text} onChange={hendleOnChange} rows="10"></textarea>
+                </div>
+
+                <button type="button" className='btn btn-primary mx-1 btn-sm' onClick={hendleOnClick} >UPPERCASE</button>
+                <button type="button" className='btn btn-primary mx-1 btn-sm' onClick={hendleLoClick} >lowercase</button>
+                <button type="button" className='btn btn-primary mx-1 btn-sm' onClick={hendleSetCase} >Sentence Case</button>
+                <button type="button" className='btn btn-primary mx-1 btn-sm' onClick={hendletrim} >Trim</button>
+                <button type="button" className='btn btn-primary mx-1 btn-sm' onClick={hendleCopy} >Copy Text</button>
+                <button type="button" className='btn btn-primary mx-1 btn-sm' onClick={hendleClear} >Clear</button>
+            </div>
+            <div className={`container my-2 text-${props.mode === 'light' ? 'dark' : 'light'}`}>
+                <h2>Your text summary</h2>
+                <p>{text.split(" ").length - 1} word and {text.length} characters</p>
+                <h2>Preview</h2>
+                <p>{text.length > 0 ? text : "Write somthing in above to preview here."}</p>
+            </div>
+        </>
+    )
+}
